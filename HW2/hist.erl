f154d0196b74800a7a5245da1b6fc21791f1d0ec
David Fischer <david@konst.fish>
2025-09-07 17:00:12 +0200

    feat(hw2): 4. hist module

---
+++ HW2/hist.erl
@@ -0,0 +1,17 @@
+-module(hist).
+-export([new/1, update/3]).
+
+% return a new history where messages from Name will be always seen as old
+new(Name) ->
+  #{Name => inf}.
+
+% check if message number N from Node is old or new. If it is old, return `old`, if it is new, return {new, Updated}
+% where Updated is the new history
+update(Node, N, History) ->
+  MaxSeen = maps:get(Node, History, 0),
+
+  if
+    MaxSeen < N ->
+      {new, maps:put(Node, N, History)};
+    true -> old
+  end.
