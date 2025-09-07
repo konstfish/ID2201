f1541a84f94d11f587bd1ca88445da7ad79e56c8
David Fischer <david@konst.fish>
2025-09-07 16:35:41 +0200

    feat(hw2): 3. router module

--- HW2/README.md
+++ HW2/README.md
@@ -7,3 +7,39 @@ At the corresponding reporting seminar, you are required to present and demonstr
 ## Links
 
 https://www.erlang.org/doc/apps/stdlib/lists.html
+https://www.erlang.org/doc/apps/stdlib/sets.html#filter/2
+
+## Test Commands
+
+### Router
+```erlang
+%% test commands
+
+I0 = router:new().
+
+P1 = spawn(fun() -> receive Msg -> io:format("P1: ~p~n", [Msg]) end end).
+P2 = spawn(fun() -> receive Msg -> io:format("P2: ~p~n", [Msg]) end end).
+R1 = make_ref().
+R2 = make_ref().
+
+I1 = router:add(london, R1, P1, I0).
+I2 = router:add(paris, R2, P2, I1).
+
+router:lookup(london, I2).
+router:lookup(paris, I2).
+router:lookup(madrid, I2).
+
+router:ref(london, I2).
+router:ref(paris, I2).
+
+router:name(R1, I2).
+router:name(R2, I2).
+
+router:list(I2).
+
+router:broadcast(hello_world, I2).
+
+I3 = router:remove(london, I2).
+router:list(I3).
+router:lookup(london, I3).
+```
---
+++ HW2/router.erl
@@ -0,0 +1,57 @@
+-module(router).
+-export([new/0, add/4, remove/2, lookup/2, ref/2, name/2, list/1, broadcast/2]).
+
+% return an empty set of interfaces
+new() ->
+  sets:new().
+
+% add a new entry to the set and return the new set of interfaces
+add(Name, Ref, Pid, Intf) ->
+   sets:add_element({Name, Ref, Pid}, Intf).
+
+% remove an entry given a name of an instance, return a new set of interfaces
+remove(Name, Intf) ->
+    case {lookup(Name, Intf), ref(Name, Intf)} of
+        {{ok, Pid}, {ok, Ref}} ->
+            sets:del_element({Name, Ref, Pid}, Intf);
+        _ -> % unchanged
+            Intf
+    end.
+
+% find the process identifier given a name, return {ok, Pid} if found, otherwise `notfound`
+lookup(Name, Intf) ->
+  case sets:to_list(sets:filter(fun({N, _Ref, _Pid}) -> N =:= Name end, Intf)) of
+    [{_Name, _Ref, Pid}] ->
+      {ok, Pid};
+    [] ->
+      notfound
+  end.
+
+% find the reference given a name and return {ok, Ref} or `notfound`
+ref(Name, Intf) ->
+  case sets:to_list(sets:filter(fun({N, _Ref, _Pid}) -> N =:= Name end, Intf)) of
+    [{_Name, Ref, _Pid}] ->
+      {ok, Ref};
+    [] ->
+      notfound
+  end.
+
+% find the name of an entry given a reference and return {ok, Name} or `notfound`
+name(Ref, Intf) ->
+  case sets:to_list(sets:filter(fun({_Name, R, _Pid}) -> R =:= Ref end, Intf)) of
+    [{Name, _Ref, _Pid}] ->
+      {ok, Name};
+    [] ->
+      notfound
+  end.
+
+% return a list with all names
+list(Intf) ->
+  lists:foldl(fun({Name, _Ref, _Pid}, Acc) -> Acc ++ [Name] end, [], sets:to_list(Intf)).
+
+% send the message to all interface processes
+broadcast(Message, Intf) ->
+  lists:map(fun({Name, _Ref, Pid}) -> 
+                io:format("sending to ~p~n", [Name]),
+                Pid ! Message 
+            end, sets:to_list(Intf)).
